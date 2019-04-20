86a62f1d7fed094b13ee61d10cf6cffc59da0283
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-04-20 15:50:30 -0500

    done

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Train-Scheduler-
-train schedule application that incorporates Firebase to host arrival and departure data
+train schedule application that incorporates momement.js for time conversion and Firebase to store arrival and departure data
+
+Users enter the train's name, destination, time of the first train, and frequency.  With some calculations, the next arrival
+
+New things learned and used
+
+- Use Firebase Database to store data
+- Use Moment.js to easily convert
+- Also used awesome-fonts, an icon, and found a js clock that displays the current time in the jumbotron
+
+**Train Scheduler can be found @:**
+
+[https://github.com/dallasappraiser/Train-Scheduler](https://github.com/dallasappraiser/Train-Scheduler "https://github.com/dallasappraiser/Train-Scheduler")
+
+and
+
+
+ [https://dallasappraiser.github.io/Train-Scheduler]( https://dallasappraiser.github.io/Train-Scheduler/. " https://dallasappraiser.github.io/Train-Scheduler/.")
--- assets/js/train.js
+++ assets/js/train.js
@@ -1,4 +1,5 @@
 // ! Initialize Firebase
+
 var config = {
   apiKey: "AIzaSyBwaQYqB5lHdxUwTgF2GsbRxWNECuQ4i2U",
   authDomain: "train-scheduler-a03c9.firebaseapp.com",
@@ -7,10 +8,12 @@ var config = {
   storageBucket: "train-scheduler-a03c9.appspot.com",
   messagingSenderId: "716373405714"
 };
-
 firebase.initializeApp(config);
 
-$("document").ready(function(event) {
+$("document").ready(function (event) {
+  // Variable for the firebase database.
+  var database = firebase.database();
+
   var trainName = "";
   var destination = "";
   var firstTrain = "";
@@ -22,7 +25,7 @@ $("document").ready(function(event) {
 
   // * collect user input *
 
-  $("#submit-train").on("click", function(event) {
+  $("#submit-train").on("click", function (event) {
     event.preventDefault();
 
     trainName = $("#train-name-input")
@@ -38,51 +41,71 @@ $("document").ready(function(event) {
       .val()
       .trim();
 
-    $("#train-name").html(trainName);
-    $("#destination").html(destination);
-    $("#frequency").html(frequency);
-    $("#next-arrival").html(currentTime);
+    // * First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
 
-    updatetime();
+    //*  Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-    clearInput();
+    //* Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+
+    //* Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // * Next Train Hour Conversion
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainTime = moment(nextTrain).format("hh:mm");
+
+    // * pushing to the database
+    database.ref().push({
+      trainName,
+      destination,
+      firstTrain,
+      frequency,
+      tMinutesTillTrain,
+      nextTrainTime,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
   });
 
+  // ? Firebase Watcher
+  database.ref().on("child_added", function (snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var snapVal = snapshot.val();
+
+    // * Add to page, append to the table, clear the form
+    createRow();
+    clearInput();
+
+    function createRow() {
+      var tRow = $("<tr>");
+      var name = $("<td>").html(snapVal.trainName);
+      var whereTO = $("<td>").html(snapVal.destination);
+      var howOften = $("<td>").html(snapVal.frequency);
+
+      var next = $("<td>").html(snapVal.nextTrainTime);
+      var til = $("<td>").html(snapVal.tMinutesTillTrain);
+
+      tRow.append(name, whereTO, howOften, next, til);
+      tRow.prependTo($('.train-table'));
+    }
+
+    // * Handle the errors *
+  }, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+  // * clear the field input after send
   function clearInput() {
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#first-train-input").val("");
     $("#frequency-input").val("");
-    console.log(18 % 5);
   }
 
-  function updatetime() {
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-    console.log("------------------------------------");
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-
-    // Minute Until Train
-    var tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    var nextTrainTime = moment(nextTrain).format("hh:mm");
-    console.log("Next Train Comes at " + nextTrainTime);
-
-    $("#next-arrival").html(nextTrainTime);
-    $("#minutes-away").html(tMinutesTillTrain);
-
-    setTimeout(updatetime, 10000);
-  }
-
+  // ! I found this somewhere and thought it would be fun to add a current clock
   function clock() {
     var date = new Date();
     var h = date.getHours();
@@ -109,6 +132,5 @@ $("document").ready(function(event) {
 
     setTimeout(clock, 1000);
   }
-
   clock();
 });
